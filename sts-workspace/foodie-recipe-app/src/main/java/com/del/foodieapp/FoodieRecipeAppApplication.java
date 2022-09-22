a6476052888c7c42fa73fbc56cefac093b2c66c8
ramanujdas <ramanujdas@localhost>
2022-09-22 14:41:55 +0530

    Recipe Microservices

---
+++ sts-workspace/foodie-recipe-app/src/main/java/com/del/foodieapp/FoodieRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.del.foodieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieRecipeAppApplication.class, args);
+	}
+
+}
